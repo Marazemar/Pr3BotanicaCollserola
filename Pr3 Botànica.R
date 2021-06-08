c159305823b042f30eca9258fc854aeae8b5a736
Marazemar <72250088+Marazemar@users.noreply.github.com>
2021-06-08 10:51:51 +0200

    2 Scripts afegits
    El script "Pr3 Botànica.R" correspont a l'exercici 3 i el script "Pr3_2 Botanica.R" és el resultat de l'exercici 5.

---
+++ Pr3 Botànica.R
@@ -0,0 +1,34 @@
+#PREGUNTA 3
+library(ggplot2)
+Alldata <- read.csv("C:/Users/azema/OneDrive/Escritorio/BH/Botànica/Pr3/InformePr3Botanica.csv", sep=";", stringsAsFactors=TRUE)
+#Sumo quantes espècies hi ha a cada parcel·la
+S1 <- sum(Alldata$Parcella == "1")
+S2 <- sum(Alldata$Parcella == "2")
+S3 <- sum(Alldata$Parcella == "3")
+S4 <- sum(Alldata$Parcella == "4")
+S5 <- sum(Alldata$Parcella == "5")
+S6 <- sum(Alldata$Parcella == "6")
+#Sumo quants individus hi ha a cada parcel·la
+N1 <- sum(Alldata$Nombre[Alldata$Parcella == "1"])
+N2 <- sum(Alldata$Nombre[Alldata$Parcella == "2"])
+N3 <- sum(Alldata$Nombre[Alldata$Parcella == "3"])
+N4 <- sum(Alldata$Nombre[Alldata$Parcella == "4"])
+N5 <- sum(Alldata$Nombre[Alldata$Parcella == "5"])
+N6 <- sum(Alldata$Nombre[Alldata$Parcella == "6"])
+#Calculo riquesa específica D=(S-1/ln(N))
+D1 <- round((S1-1)/log(N1), digits = 2)
+D2 <- round((S2-1)/log(N2), digits = 2)
+D3 <- round((S3-1)/log(N3), digits = 2)
+D4 <- round((S4-1)/log(N4), digits = 2)
+D5 <- round((S5-1)/log(N5), digits = 2)
+D6 <- round((S6-1)/log(N6), digits = 2)
+#Data per fer el plot
+Diversitat <- c(D1, D2, D3, D4, D5, D6)
+Distància <- c(0,5,10,15,20, 25)
+DistDiv <- data.frame (Distància, Diversitat)
+#Plot:
+ggplot (DistDiv, aes (Distància, Diversitat)) + geom_line(color="chartreuse3") + geom_point(size=3, shape=20, fill="white")+ theme_light() + xlim (0,25) + ylim (0,3.5) + xlab("Distància al camí (m)") + ylab("Diversitat específica") + labs(title = "Distància al camí en vers la diversitat específica", subtitle = "M del Mar Azemar, Botànica, 2020/21") + theme(plot.title = element_text(hjust = 0.5, size = 15, face = "bold"), plot.subtitle = element_text(hjust = 0.5)) + stat_smooth(formula = y ~ x, method = 'lm', color = "aquamarine3", se = FALSE)
+
+
+
+        
---
+++ Pr3_2 Botanica.R
@@ -0,0 +1,9 @@
+#Plot:
+library(ggplot2)
+Alldata <- read.csv("C:/Users/azema/OneDrive/Escritorio/BH/Botànica/Pr3/InformePr3Botanica.csv", sep=";", stringsAsFactors=TRUE)
+Distancia <- factor(Alldata$Distància.al.camí..m.)
+ggplot(Alldata, aes(x=Distancia, y=Nombre, group = Espècie, colour =Espècie )) + geom_line()  + geom_point(size=3, shape=20, fill="white") + theme_light() + labs(title = "Distància al camí en vers el nombre d'individus per espècies:", subtitle = "M del Mar Azemar, Botànica, 2020/21") + xlab("Distància al camí (m)") + 
+theme(plot.title = element_text(hjust = 0.5, size = 15, face = "bold"), plot.subtitle = element_text(hjust = 0.5)) + ylab("Nombre d'individus") + ylim(0,50)
+
+         
+               
